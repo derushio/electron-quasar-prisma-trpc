57c41e38103ec76e8f9661cbe72f043057a20bcd
derushio <derushio@gmail.com>
2022-12-28 23:11:45 +0900

    superjsonに対応

--- src-electron/backend/trpc/index.ts
+++ src-electron/backend/trpc/index.ts
@@ -1,4 +1,7 @@
 import { Context } from '#/backend/trpc/context';
 import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
 
-export const t = initTRPC.create<Context>();
+export const t = initTRPC.create<Context>({
+  transformer: superjson,
+});
