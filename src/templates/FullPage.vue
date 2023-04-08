04051b38b04f5b3dfa32dd17bf6bf3ccfc9513df
derushio <derushio@aiqlab.com>
2023-04-08 14:56:19 +0900

    add: fullpage

---
+++ src/templates/FullPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="full-height column overflow-hidden">
+    <slot />
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+// pass;
+</script>
+
+<style lang="scss">
+html,
+body,
+#q-app,
+.q-layout,
+.q-page-container {
+  height: 100%;
+}
+</style>
