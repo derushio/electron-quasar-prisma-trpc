6ce47019e49bde8d40b7c19bf1a18496fe278621
derushio <derushio@aiqlab.com>
2023-04-09 20:52:13 +0900

    fix type

--- src-electron/backend/trpc/index.ts
+++ src-electron/backend/trpc/index.ts
@@ -1,7 +1,6 @@
-import { Context } from '#/backend/trpc/context';
 import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 
-export const t = initTRPC.create<Context>({
+export const t = initTRPC.create({
   transformer: superjson,
 });
--- src/domains/trpc/trpc.ts
+++ src/domains/trpc/trpc.ts
@@ -3,7 +3,7 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  transformer: superjson as never,
+  transformer: superjson,
   links: [
     httpBatchLink({
       url: `http://localhost:${process.env.BACKEND_PORT}/trpc`,
